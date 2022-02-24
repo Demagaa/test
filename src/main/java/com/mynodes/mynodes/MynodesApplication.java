677f606dfe8a25774076670aa0449b50bdd86e26
Demagaa <90135638+Demagaa@users.noreply.github.com>
2022-02-24 18:13:55 +0100

    test commit

---
+++ src/main/java/com/mynodes/mynodes/MynodesApplication.java
@@ -0,0 +1,13 @@
+package com.mynodes.mynodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynodesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MynodesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mynodes/mynodes/MynodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mynodes.mynodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MynodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
